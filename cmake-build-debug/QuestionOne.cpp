22e292f1aae1adbed074d869c611906b10d1a899
Direnc Can Karahan <direnccankarahan@gmail.com>
2023-03-09 15:24:51 +0300

    asd

--- cmake-build-debug/QuestionOne.cpp
+++ cmake-build-debug/QuestionOne.cpp
@@ -1,3 +1,16 @@
-//
-// Created by canka on 9.03.2023.
-//
+
+
+int questionOne() {
+    string x;
+    double y;
+
+    std::cout << "What is your name?" << std::endl;
+    cin >> x;
+    cout << "Hello " << x << "." << endl;
+
+    cout << "What is your Student ID?" << endl;
+    cin >> y;
+    cout << "Your Student ID is " << y << "." << endl;
+
+    return 0;
+}
